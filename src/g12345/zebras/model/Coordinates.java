013991fe00cd932355965ef64954b97864aff10e
mneli <m.nyeem@elium.academy>
2017-03-23 02:47:11 +0100

    Pseudo structure Coordinates added

---
+++ src/g12345/zebras/model/Coordinates.java
@@ -0,0 +1,41 @@
+package g12345.zebras.model;
+
+/**
+ *
+ * @author g12345
+ */
+public class Coordinates {
+
+    private final int row;
+    private final int column;
+
+    /**
+     * Get the value of row
+     *
+     * @return the value of row
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * Get the value of column
+     *
+     * @return the value of column
+     */
+    public int getColumn() {
+        return column;
+    }
+
+    /**
+     * Construct an instance of Coordinates
+     *
+     * @param row
+     * @param column
+     */
+    public Coordinates(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+}
