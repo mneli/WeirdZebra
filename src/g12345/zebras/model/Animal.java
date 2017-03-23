c6ced4fde1a5b32cbb4eeb4b9552b31fafea0205
mneli <m.nyeem@elium.academy>
2017-03-23 02:53:54 +0100

    Class Animal added

---
+++ src/g12345/zebras/model/Animal.java
@@ -0,0 +1,41 @@
+package g12345.zebras.model;
+
+/**
+ *
+ * @author g12345
+ */
+public class Animal {
+
+    private final Species species;
+    private final Color color;
+
+    /**
+     * Get the value of species
+     *
+     * @return the value of species
+     */
+    public Species getSpecies() {
+        return species;
+    }
+
+    /**
+     * Get the value of color
+     *
+     * @return the value of color
+     */
+    public Color getColor() {
+        return color;
+    }
+
+    /**
+     * Construct an instance of Animal
+     *
+     * @param species
+     * @param color
+     */
+    public Animal(Species species, Color color) {
+        this.species = species;
+        this.color = color;
+    }
+
+}
